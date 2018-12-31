942782670ee420011ba90f1f4fbe237404bcdbd0
Andrea Williams <anw0514@gmail.com>
2018-12-31 16:26:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else
-    newar = "The line is currently"
-    array.each_with_index { |element, index| newar.push("#{index + 1}. #{element} ") }
+    str = "The line is currently "
+    array.each_with_index { |element, index| str ("#{index + 1}. #{element} ") }
     newar.each { |item| puts item.chomp }
   end
 end
